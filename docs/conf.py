737ac1ced8ed3ab740e998001f2c5fde50f95a47
chiefenne <andreas.ennemoser@gmail.com>
2016-10-20 12:21:27 +0200

    type in project name

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'PyAaero'
+project = u'PyAero'
 copyright = u'2016, Andreas Ennemoser'
 author = u'Andreas Ennemoser'
 
