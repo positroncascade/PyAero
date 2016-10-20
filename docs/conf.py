52ece1410343b1db8e05acfc68e41176f4faf4b9
chiefenne <andreas.ennemoser@gmail.com>
2016-10-20 08:51:52 +0200

    deleting directory

--- docs/conf.py
+++ docs/conf.py
@@ -12,13 +12,18 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
+# this allows to use the readthedocs theme also locally
+import sphinx_rtd_theme
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
 
 # -- General configuration ------------------------------------------------
 
@@ -121,7 +126,10 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'default'
+# html_theme = 'default'
+
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
