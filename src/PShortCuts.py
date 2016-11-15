7a3a736b332ca07448d82982bc87352c09e9034f
Andreas Ennemoser <andreas.ennemoser@avl.com>
2016-11-15 13:45:11 +0100

    new class for adding general shortcuts which call handlers from PGuiSlots

---
+++ src/PShortCuts.py
@@ -0,0 +1,27 @@
+
+from PyQt4 import QtGui, QtCore
+
+class PShortCuts(object):
+    """docstring for ClassName """
+    def __init__(self, parent):
+        if not isinstance(parent, QtGui.QMainWindow):
+            raise TypeError('parent must be a MainWindow')
+        super(PShortCuts, self).__init__()
+        self.parent = parent
+    
+    def addShortcut(self, shortcut, slot):
+        """Add a shortcut to a slot (event handler)
+        
+        Args:
+            shortcut (STRING): Something like 'ALT+m'
+            slot (STRING): Method of PGuiSlots
+        
+        Returns:
+            object: QShortcut object
+        """
+
+        guislot = getattr(self.parent.slots, slot)
+
+        sc = QtGui.QShortcut(QtGui.QKeySequence(shortcut), self.parent,
+                        guislot)
+        return sc
