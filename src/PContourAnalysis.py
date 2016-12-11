7d238a5a47f45e77e9fa8e505f7113bb9dec6f79
Andreas Ennemoser <andreas.ennemoser@aon.at>
2016-12-12 00:14:41 +0100

    removed splining and refining

--- src/PContourAnalysis.py
+++ src/PContourAnalysis.py
@@ -1,4 +1,3 @@
-import copy
 
 from matplotlib.backends.backend_qt4agg \
     import FigureCanvasQTAgg as FigureCanvas
@@ -7,10 +6,8 @@ from matplotlib.backends.backend_qt4agg \
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import numpy as np
-import scipy.interpolate as si
 
 from PyQt4 import QtGui, QtCore
-from PUtils import Utils
 
 import PLogger as logger
 
@@ -112,237 +109,6 @@ class ContourAnalysis(QtGui.QFrame):
         # necessary so that changing between gradient, radius, etc. works
         plt.clf()
 
-    def spline(self, x, y, points=200, degree=2, evaluate=False):
-        """Interpolate spline through given points
-
-        Args:
-            spline (int, optional): Number of points on the spline
-            degree (int, optional): Degree of the spline
-            evaluate (bool, optional): If True, evaluate spline just at
-                                       the coordinates of the knots
-        """
-
-        # interpolate B-spline through data points
-        # returns knots of control polygon
-        # tck ... tuple (t,c,k) containing the vector of knots,
-        # the B-spline coefficients, and the degree of the spline.
-        # u ... array of the parameters for each knot
-        tck, u = si.splprep([x, y], s=0, k=degree)
-
-        # number of points on interpolated B-spline (parameter t)
-        t = np.linspace(0.0, 1.0, points)
-
-        # if True, evaluate spline just at the coordinates of the knots
-        if evaluate:
-            t = u
-
-        # evaluate B-spline at given parameters
-        # der=0: returns point coordinates
-        coo = si.splev(t, tck, der=0)
-
-        # evaluate 1st derivative at given parameters
-        der1 = si.splev(t, tck, der=1)
-
-        # evaluate 2nd derivative at given parameters
-        der2 = si.splev(t, tck, der=2)
-
-        self.spline_data = [coo, u, t, der1, der2, tck]
-
-        return self.spline_data
-
-    def refine(self, tol=170.0, recursions=0, verbose=False):
-        """Recursive refinement with respect to angle criterion (tol).
-        If angle between two adjacent line segments is less than tol,
-        a recursive refinement of the contour is performed until
-        tol is met.
-
-        Args:
-            tol (float, optional): Angle between two adjacent contour segments
-            recursions (int, optional): NO USER INPUT HERE
-                                        Needed just for level information
-                                        during recursions
-            verbose (bool, optional): Activate logger messages
-        """
-
-        # self.spline_data = [coo, u, t, der1, der2, tck]
-        xx, yy = self.spline_data[0]
-        t = self.spline_data[2]
-        tck = self.spline_data[5]
-
-        if verbose:
-            logger.log.info('\nPoints before refining: %s \n' % (len(xx)))
-
-        xn = copy.deepcopy(xx)
-        yn = copy.deepcopy(yy)
-        tn = copy.deepcopy(t)
-
-        j = 0
-        refinements = 0
-        first = True
-        refined = dict()
-
-        for i in range(len(xx) - 2):
-            refined[i] = False
-
-            # angle between two contour line segments
-            a = np.array([xx[i], yy[i]])
-            b = np.array([xx[i + 1], yy[i + 1]])
-            c = np.array([xx[i + 2], yy[i + 2]])
-            angle = Utils.angle_between(a - b, c - b, degree=True)
-
-            if angle < tol:
-
-                refined[i] = True
-                refinements += 1
-
-                # parameters for new points
-                t1 = (t[i] + t[i + 1]) / 2.
-                t2 = (t[i + 1] + t[i + 2]) / 2.
-
-                # coordinates of new points
-                p1 = si.splev(t1, tck, der=0)
-                p2 = si.splev(t2, tck, der=0)
-
-                # insert points and their parameters into arrays
-                if i > 0 and not refined[i - 1]:
-                    xn = np.insert(xn, i + 1 + j, p1[0])
-                    yn = np.insert(yn, i + 1 + j, p1[1])
-                    tn = np.insert(tn, i + 1 + j, t1)
-                    j += 1
-                xn = np.insert(xn, i + 2 + j, p2[0])
-                yn = np.insert(yn, i + 2 + j, p2[1])
-                tn = np.insert(tn, i + 2 + j, t2)
-                j += 1
-
-                if first and recursions > 0:
-                    if verbose:
-                        logger.log.info('Recursion level: %s \n' %
-                                        (recursions))
-                    first = False
-
-                if verbose:
-                    logger.log.info('Refining between %s %s, Tol=%05.1f Angle=%05.1f\n'
-                                    % (i, i + 1, tol, angle))
-
-        if verbose:
-            logger.log.info('Points after refining: %s' % (len(xn)))
-
-        # update coordinate array, including inserted points
-        self.spline_data[0] = (xn, yn)
-        # update parameter array, including parameters of inserted points
-        self.spline_data[2] = tn
-
-        # this is the recursion :)
-        if refinements > 0:
-            self.refine(tol, recursions + 1, verbose)
-
-        # stopping from recursion if no refinements done in this recursion
-        else:
-
-            # update derivatives, including inserted points
-            self.spline_data[3] = si.splev(tn, tck, der=1)
-            self.spline_data[4] = si.splev(tn, tck, der=2)
-
-            if verbose:
-                logger.log.info('No more refinements.')
-                logger.log.info('\nTotal number of recursions: %s'
-                                % (recursions - 1))
-            return
-
-    def getUpperLower(self):
-        """Split contour in upper and lower parts
-
-        Returns:
-            TYPE: Coordinates of upper and lower contours
-        """
-        # leading edge radius
-        rc, xc, yc, xle, yle, le_id = self.getLeRadius()
-
-        x, y = self.spline_data[0]
-        upper = (x[:le_id + 1], y[:le_id + 1])
-        lower = (x[le_id:], y[le_id:])
-
-        return upper, lower
-
-    def trailingEdge(self, blend=0.3, ex=3.0, thickness=0.6, side='both'):
-        """Implement a finite trailing edge thicknes into the original
-        contour
-
-        Args:
-            blend (float, optional): Length to blend the TE
-            ex (float, optional): Exponent that modifies the blending
-                                  curve
-            thickness (float, optional): TE thickness
-            side (str, optional): Defines if blending is done on upper,
-                                  lower or both sides
-
-        Returns:
-            tuple: Updated spline coordinates
-        """
-        upper, lower = self.getUpperLower()
-        xu = copy.copy(upper[0])
-        yu = copy.copy(upper[1])
-        xl = copy.copy(lower[0])
-        yl = copy.copy(lower[1])
-        xnu = copy.copy(xu)
-        ynu = copy.copy(yu)
-        xnl = copy.copy(xl)
-        ynl = copy.copy(yl)
-        if side == 'upper' or side == 'both':
-            xnu, ynu = self.trailing(xu, yu, blend, ex, thickness,
-                                     side='upper')
-        if side == 'lower' or side == 'both':
-            xnl, ynl = self.trailing(xl, yl, blend, ex, thickness,
-                                     side='lower')
-        xt = np.concatenate([xnu, xnl[1:]])
-        yt = np.concatenate([ynu, ynl[1:]])
-        self.spline_data[0] = (xt, yt)
-
-        return self.spline_data[0]
-
-    def trailing(self, xx, yy, blend, ex, thickness, side='upper'):
-        xmin = np.min(xx)
-        xmax = np.max(xx)
-        chord = xmax - xmin
-        thickness = chord * thickness / 100.0
-        blend_points = np.where(xx > (1.0 - blend) * xmax)
-        x = copy.copy(xx)
-        y = copy.copy(yy)
-        if side == 'upper':
-            signum = 1.0
-            a = np.array([x[1] - x[0], y[1] - y[0]])
-        elif side == 'lower':
-            signum = -1.0
-            a = np.array([x[-2] - x[-1], y[-2] - y[-1]])
-        e = Utils.unit_vector(a)
-        n = np.array([e[1], -e[0]])
-        shift = 0.5 * thickness
-        for i in blend_points:
-            shift_blend = (x[i] - xmax * (1.0 - blend)) / \
-                          (xmax * blend)
-            x[i] = x[i] + signum * n[0] * shift_blend**ex * shift
-            y[i] = y[i] + signum * n[1] * shift_blend**ex * shift
-        return x, y
-
-    def writeContour(self):
-
-        xr = self.raw_coordinates[0]
-        xc = self.coordinates[0]
-        yc = self.coordinates[1]
-        s = '# Spline with {0} points based on initial contour'.format(len(xc))
-        s1 = '({0} points)\n'.format(len(xr))
-        info = s + s1
-
-        with open(self.name + '_spline_' + str(len(xc)) + '.dat', 'w') as f:
-            f.write('#\n')
-            f.write('# Airfoil: ' + self.name + '\n')
-            f.write('# Created from ' + self.filename + '\n')
-            f.write(info)
-            f.write('#\n')
-            for i in range(len(xc)):
-                data = '{:10.8f} {:10.8f} \n'.format(xc[i], yc[i])
-                f.write(data)
-
     def getCurvature(self):
         """Curvature and radius of curvature of a parametric curve
 
@@ -382,29 +148,11 @@ class ContourAnalysis(QtGui.QFrame):
         self.curvature_data = [gradient, C, R, xc, yc]
         return
 
-    def analyze(self, tolerance, spline_points, plot, reset=True):
+    def analyze(self, plot, reset=True):
 
         if reset:
             # logger.stack(logger.log)
             self.reset()
-
-        # FIXME
-        # FIXME get airfoil coordinates here from scene item
-        # FIXME either selected airfoil or only loaded airfoil if any
-        # FIXME
-
-        # interpolate a spline through the raw contour points
-        x, y = self.parent.airfoil.raw_coordinates
-        self.spline(x, y, points=spline_points, degree=2)
-
-        # refine the contour in order to meet the tolerance
-        self.refine(tol=tolerance, verbose=False)
-
-        # redo spline on refined contour
-        # spline only evaluated at refined contour points (evaluate=True)
-        coo, u, t, der1, der2, tck = self.spline_data
-        x, y = coo
-        self.spline(x, y, points=spline_points, degree=2, evaluate=True)
 
         # get specific curve properties
         self.getCurvature()
