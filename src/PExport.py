c2ed1e3d808ff04a9a81d6c2abfd6fe96dac7bc6
Andreas Ennemoser <andreas.ennemoser@avl.com>
2017-01-16 16:14:45 +0100

    prepare connect mesh blocks

--- src/PExport.py
+++ src/PExport.py
@@ -1,75 +1,68 @@
+import numpy as np
+import scipy.spatial as ssp
 
 
 class Export(object):
     """docstring for Export"""
-    def __init__(self, arg):
+    def __init__(self, blocks):
         super(Export, self).__init__()
-        self.arg = arg
+        self.blocks = blocks
 
-    def exportFLMA(self, blocks, name='', depth=0.1):
+    def getNearestNeighbours(set1, set2, radius):
+        """Nearest neighbour search. Get all indices of points in d1
+        which are within distance radius to d2.
 
-        lines = list()
+        Args:
+            set1 (list): Points in set1 [(x1, y1), (x2, y2), ...]
+            set2 (list): Points in set2 [(x1, y1), (x2, y2), ...]
+            radius (float): Search radius
 
-        numvertex = '8'
+        Return (list): List with indices of set1 which are within a distance
+                       less than radius to set2
+        """
+        tree_1 = ssp.cKDTree(set1)
+        tree_2 = ssp.cKDTree(set2)
+        idx = tree_2.query_ball_tree(tree_1, radius, p=2., eps=0)
+
+        indices = [e[0] for e in idx if e]
+
+        return indices
+
+    def connectBlocks(self, name='', depth=0.1):
+
+        vertices = 0
+        ulines = dict()
+        connectivity = dict()
+        I = 0
+        J = 0
 
         # loop over all individual mesh blocks
-        for block in blocks:
+        for block in self.blocks:
 
             U, V = block.getDivUV()
-            points = (U + 1) * (V + 1)
+            vertices += (U + 1) * (V + 1)
+            ulines.setdefault(block.name, []).append(block.getULines())
 
-            # write number of points to FLMA file (*2 for z-direction)
-            lines.append(str(2 * points) + '\n')
-
-            signum = -1.
-
-            # write x-, y- and z-coordinates to FLMA file
-            # loop 1D direction (symmetry)
-            for j in range(2):
-                for uline in block.getULines():
-                    for i in range(len(uline)):
-                        lines.append(str(uline[i][0]) + ' ' +
-                                     str(uline[i][1]) +
-                                     ' ' + str(signum * depth / 2.0) + ' ')
-                signum = 1.
-
-            # write number of cells to FLMA file
-            cells = U * V
-            lines.append('\n' + str(cells) + '\n')
-
-            # write cell connectivity to FLMA file
+            # cell connectivity
             up = U + 1
             vp = V + 1
             for i in range(U):
                 for j in range(V):
 
-                    p1 = j * up + i + 1
-                    p2 = (vp + j) * up + i + 1
+                    I += i
+                    J += j
+
+                    p1 = J * up + I + 1
+                    p2 = (vp + J) * up + I + 1
                     p3 = p2 - 1
                     p4 = p1 - 1
-                    p5 = (j + 1) * up + i + 1
-                    p6 = (vp + j + 1) * up + i + 1
+                    p5 = (J + 1) * up + I + 1
+                    p6 = (vp + J + 1) * up + I + 1
                     p7 = p6 - 1
                     p8 = p5 - 1
 
-                    connectivity = str(p1) + ' ' + str(p2) + ' ' + str(p3) + \
-                        ' ' + str(p4) + ' ' + str(p5) + ' ' + str(p6) + \
-                        ' ' + str(p7) + ' ' + str(p8) + '\n'
+                    connectivity.setdefault(block.name, []).append(
+                        (p1, p2, p3, p4, p5, p6, p7, p8))
 
-                    lines.append(numvertex + '\n')
-                    lines.append(connectivity)
+        for block in len(self.blocks()):
 
-            # write FIRE element type (FET) to FLMA file
-            fetHEX = '5'
-            lines.append('\n' + str(cells) + '\n')
-            for i in range(cells):
-                lines.append(fetHEX + ' ')
-            lines.append('\n\n')
-
-            # write FIRE selections to FLMA file
-            lines.append('0')
-
-        filename = name
-        with open(filename, 'w') as f:
-            for line in lines:
-                f.write(line)
