371c29d02a0e07eafef119a776b4b4ef6454678b
Andreas Ennemoser <andreas.ennemoser@aon.at>
2017-01-24 00:26:02 +0100

    refactoring; not ready

--- src/PExport.py
+++ src/PExport.py
@@ -9,8 +9,9 @@ class Export(object):
         self.blocks = blocks
 
     def getNearestNeighbours(set1, set2, radius=0.001):
-        """Nearest neighbour search. Get all indices of points in d1
-        which are within distance radius to d2.
+        """Nearest neighbour search using KD-trees. Get all indices of
+        points in set1 which are within distance radius to set2.
+        And vice versa.
 
         Args:
             set1 (list): Points in set1 [(x1, y1), (x2, y2), ...]
@@ -22,11 +23,13 @@ class Export(object):
         """
         tree_1 = ssp.cKDTree(set1)
         tree_2 = ssp.cKDTree(set2)
-        idx = tree_2.query_ball_tree(tree_1, radius, p=2., eps=0)
+        id1 = tree_2.query_ball_tree(tree_1, radius, p=2., eps=0)
+        id2 = tree_1.query_ball_tree(tree_2, radius, p=2., eps=0)
 
-        indices = [e[0] for e in idx if e]
+        indices_1 = [e[0] for e in id1 if e]
+        indices_2 = [e[0] for e in id2 if e]
 
-        return indices
+        return indices_1, indices_2
 
     def connectBlocks(self, name='', depth=0.1):
 
@@ -64,13 +67,13 @@ class Export(object):
                     connectivity.setdefault(block.name, []).append(
                         (p1, p2, p3, p4, p5, p6, p7, p8))
 
-        for id, block in enumerate(self.blocks):
-            if id == len(self.blocks) - 2:
-                break
-            points_1 = self.getPoints(self.blocks[id])
-            points_2 = self.getPoints(self.blocks[id+1])
-            nn = self.getNearestNeighbours(points_1, points_2, radius=0.001)
-            nnrev = self.getNearestNeighbours(points_2, points_1, radius=0.001)
+        number_of_blocks = len(self.blocks)
+        for block in range(number_of_blocks-1):
+            points_1 = self.getPoints(self.blocks[block])
+            points_2 = self.getPoints(self.blocks[block+1])
+            # indices of points_1 within radius to points_2 and vice versa
+            nn_12, nn_21 = self.getNearestNeighbours(points_1, points_2,
+                                                     radius=0.001)
 
     def getPoints(self, block):
         points = list()
